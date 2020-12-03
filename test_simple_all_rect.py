c21a15be1751632d9aeadda0d14e69deed32dab0
Rusty Holleman <rustychris@gmail.com>
2020-12-03 11:10:08 -0500

    Reconfig N pond, move to orthogonal triangles

--- test_simple_all_rect.py
+++ test_simple_all_rect.py
@@ -28,7 +28,7 @@ six.moves.reload_module(triangulate_hole)
 six.moves.reload_module(orthogonalize)
 six.moves.reload_module(quads)
 
-gen_src=unstructured_grid.UnstructuredGrid.read_pickle('grid_lagoon-v17.pkl')
+gen_src=unstructured_grid.UnstructuredGrid.read_pickle('grid_lagoon-v18.pkl')
 
 ##
 if 0:
@@ -136,10 +136,10 @@ sqg.execute()
 g=sqg.g_final
 g.renumber()
 
-g.write_pickle('all-quads-v17.pkl',overwrite=True)
+g.write_pickle('all-quads-v18.pkl',overwrite=True)
 
 ## 
-g=unstructured_grid.UnstructuredGrid.read_pickle('all-quads-v17.pkl')
+g=unstructured_grid.UnstructuredGrid.read_pickle('all-quads-v18.pkl')
 
 # Add the non-cell edges of gen_src back into g:
 gen_src_tri=gen_src.copy()
@@ -180,7 +180,7 @@ g.renumber(reorient_edges=False)
 
 g_orig=g
 
-g_orig.write_pickle('quads_and_lines-v17.pkl',overwrite=True)
+g_orig.write_pickle('quads_and_lines-v18.pkl',overwrite=True)
 ##
 
 plt.figure(1).clf()
@@ -188,7 +188,7 @@ g_orig.plot_edges(lw=0.5,color='k')
 
 ##
 
-g=unstructured_grid.UnstructuredGrid.read_pickle('quads_and_lines-v17.pkl')
+g=unstructured_grid.UnstructuredGrid.read_pickle('quads_and_lines-v18.pkl')
 
 # Calculate a global apollonius scale field.  Otherwise some of the
 # internal edges can't see nearby short edges, and we get mismatches.
@@ -198,19 +198,14 @@ e2c=g.edge_to_cells(recalc=True)
 # I want edges on the boundary of the quad regions
 j_sel=(e2c.min(axis=1)<0) & (e2c.max(axis=1)>=0)
 
-# Refine this
+# Refine this by ignoring edges that won't be triangulated anyway.
 # This boundary doesn't care about cells, safe to use with a partial
 # grid.
 boundary=g.boundary_cycle()
-
 for a,b in zip(boundary,np.roll(boundary,-1)):
     j=g.nodes_to_edge([a,b])
     j_sel[j]=False
     
-plt.figure(1).clf()
-g.plot_edges(color='k',lw=0.4)
-g.plot_edges(mask=j_sel,color='tab:red',lw=1.5)
-
 ## 
 el=g.edges_length()
 ec=g.edges_center()
@@ -219,83 +214,81 @@ from stompy.spatial import field
 density=field.PyApolloniusField(X=ec[j_sel],F=el[j_sel],
                                 redundant_factor=0.95)
                                 
-## 
+##
+
 # Fill some holes!
 # th_kwargs=dict(method='gmsh',density=density)
 th_kwargs=dict(method='front',density=density,
                method_kwargs=dict(reject_cc_outside_cell=True))
 
 from stompy.grid import triangulate_hole
+
+seed_points=[
+    [552498., 4125123.], # x_north_pond
+    [552384., 4124450.], # x_lagoon_shallow
+    [552841., 4124582.], # x_north_marsh
+    [552516., 4124182.], # x_butano_lagoon
+    [552607., 4123680.], # x_butano_marsh_w
+    [552905., 4123225.], # x_butano_marsh_s
+    [552771., 4124233.], # x_delta_marsh
+    [552844., 4123945.], # x_delta_marsh_s
+    [553257., 4123782.], # x_pesc_roundhill
+    [553560., 4123089.], # x_butano_se
+    [552379., 4124697.], # x_nmarsh_west
+    [552323., 4124492.], # x_lagoon_north
+    [552226., 4124428.], # x_lagoon_south
+    [552547., 4123962.], # butano off-channel storage
+    [552581., 4123866.], # another butano off-channel storage
+    [552596., 4123395.], # various in Butano Marsh
+    [552576., 4123458.], # Funny intersection place
+    [553116., 4123723.], # extra point in butano marsh w/ tangent
+    [552746., 4123550.], # butano between old and new.
+    [552331., 4124869.], # small area on west side of N Pond channel
+]
+
+failures=[]
+
+## 
 g_new=g
-x_north_pond=[552498., 4125123.]
-g_new=triangulate_hole.triangulate_hole(g_new,seed_point=x_north_pond,**th_kwargs)
+for seed in seed_points:
+    c=g_new.select_cells_nearest(seed,inside=True)
+    if c is not None:
+        print("Point %s already filled"%seed)
+        continue
+    print("Will fill this one")
 
-x_lagoon_shallow=[552384., 4124450.]
-g_new=triangulate_hole.triangulate_hole(g_new,seed_point=x_lagoon_shallow,**th_kwargs)
-
-x_north_marsh=[552841., 4124582.]
-g_new=triangulate_hole.triangulate_hole(g_new,seed_point=x_north_marsh,**th_kwargs)
-
-x_butano_lagoon=[552516., 4124182.]
-g_new=triangulate_hole.triangulate_hole(g_new,seed_point=x_butano_lagoon,**th_kwargs)
-
-x_butano_marsh_w=[552607., 4123680.]
-g_new=triangulate_hole.triangulate_hole(g_new,seed_point=x_butano_marsh_w,**th_kwargs)
-
-x_butano_marsh_s=[552905., 4123225.]
-g_new=triangulate_hole.triangulate_hole(g_new,seed_point=x_butano_marsh_s,**th_kwargs)
-
-# g_new.write_pickle('prebug.pkl',overwrite=True)
-# # six.moves.reload_module(unstructured_grid)
-# g_new=unstructured_grid.UnstructuredGrid.read_pickle('prebug.pkl')
-
-x_delta_marsh=[552771., 4124233.]
-g_new=triangulate_hole.triangulate_hole(g_new,seed_point=x_delta_marsh,**th_kwargs)
-
-x_delta_marsh_s=[552844., 4123945.]
-g_new=triangulate_hole.triangulate_hole(g_new,seed_point=x_delta_marsh_s,**th_kwargs)
-
-g_new.write_pickle('in-progress-v17-delta_marsh_s.pkl',overwrite=True)
-
-#g_new=unstructured_grid.UnstructuredGrid.read_pickle('in-progress-v17-delta_marsh_s.pkl')
-#plt.figure(1).clf()
-#g_new.plot_edges()
-
-x_pesc_roundhill=[553257., 4123782.]
-g_new=triangulate_hole.triangulate_hole(g_new,seed_point=x_pesc_roundhill,**th_kwargs)
-
-x_butano_se=[553560., 4123089.]
-# This one is failing.  Probably because the tangent connection is too tricky
-# for triangulate_hole.  For now, add another edge to segment the marsh.
-g_new=triangulate_hole.triangulate_hole(g_new,seed_point=x_butano_se,**th_kwargs)
-
-x_nmarsh_west=[552379., 4124697.]
-g_new=triangulate_hole.triangulate_hole(g_new,seed_point=x_nmarsh_west,**th_kwargs)
-
-x_lagoon_north=[552323., 4124492.]
-g_new=triangulate_hole.triangulate_hole(g_new,seed_point=x_lagoon_north,**th_kwargs)
-
-x_lagoon_south=[552226., 4124428.]
-g_new=triangulate_hole.triangulate_hole(g_new,seed_point=x_lagoon_south,**th_kwargs)
-
-g_new.write_pickle('in-progress-v17-lagoon_south.pkl',overwrite=True)
-
-# Need to add a few more seed points.
-for seed in [[552547., 4123962.], # butano off-channel storage
-             [552581., 4123866.], # another butano off-channel storage
-             [552596., 4123395.], # various in Butano Marsh
-             [552576., 4123458.], # Funny intersection place
-             [553116., 4123723.], # extra point in butano marsh w/ tangent
-             [552746., 4123550.]
-]:
-    g_new=triangulate_hole.triangulate_hole(g_new,seed_point=seed,**th_kwargs)
+    result=triangulate_hole.triangulate_hole(g_new,seed_point=seed,**th_kwargs)
+    if isinstance(result,unstructured_grid.UnstructuredGrid):
+        g_new=result
+    else:
+        print("-----Fail on point: %s-----"%seed)
+        failures.append( result )
+        
+## 
+# 16 of 20 succeed, 4 failures.
+# 0: bad telescoping leads to doubled edge.
+# 1: around roundhill. It's just too thin here. Can I push
+#    the quads to the boundary?
+# 2: south side of the tangent join, quad resolution doesn't match
+# 3: west n pond entry angle at n end too shallow.
 
 plt.figure(1).clf()
-g_new.plot_edges(color='tab:blue',lw=0.7)
+failures[3].grid.plot_edges(lw=0.7)
+g_new.plot_edges(lw=0.5,alpha=0.3,color='0.6')
+plt.axis('tight')
+plt.axis('equal')
+
+##
+g_new.write_pickle('v18-successes.pkl')
+
+        
+##         
+plt.figure(1).clf()
+g_new.plot_edges(color='tab:brown',lw=0.7)
 plt.axis('tight')
 plt.axis('equal')
 
 g_new.renumber()
-g_new.write_ugrid('quad_tri_v17frontcc.nc',overwrite=True)
+g_new.write_ugrid('quad_tri_v18frontcc.nc',overwrite=True)
 
-##
+
